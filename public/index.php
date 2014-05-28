0beb6d503a2ca0b04b54210e08d19bd0350fb783
Matt Henry <henrymatt@me.com>
2014-05-28 18:45:35 +0900

    Removing index.php content

--- public/index.php
+++ public/index.php
@@ -3,18 +3,6 @@
 		<link rel="stylesheet" href="styles/main.css">
 	</head>
 	<body>
-		<div id="header"><h1>Header</h1></div>
-		<div id="example">Example 5-1</div>
-		
-		<div id="content">
-			<h1>SQL Connection Test</h1>
-			<form action="seriesList.php" method="POST">
-				<fieldset class="center">
-					<input type="submit" value="Connect to MySQL" />
-				</fieldset>
-			</form>
-		</div>
-		
-		<div id="footer"></div>
+
 	</body>
 </html>
