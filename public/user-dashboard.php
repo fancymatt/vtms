a5335c3c2fb7f90242b682a31bd8fa85d5207203
Matt Henry <henrymatt@me.com>
2014-06-01 14:52:34 +0900

    Allows edit of all user information, improves visuals, requires password to edit.

--- public/user-dashboard.php
+++ public/user-dashboard.php
@@ -6,15 +6,31 @@
 	$all_time_zones = DateTimeZone::listIdentifiers();
 	
 	if($_POST['user_edited']) {
+		
 		$pw1 = $db->escape_value($_POST['user_password']);
 		$pw2 = $db->escape_value($_POST['user_password_confirm']);
-		if($pw1 == $pw2) {
-			$password = $db->password_encrypt($pw2);
-			$current_user->user_password = $password;
+		$old_pw = $db->escape_value($_POST['user_old_password']);
+		$username = $db->escape_value($_POST['user_name']);
+		$time_zone = $db->escape_value($_POST['user_time_zone']);
+	
+		$found_user = User::attempt_login($current_user->user_name, $old_pw);
+		
+		if ($found_user) {
+			$current_user->user_name = $username;
+			$current_user->time_zone = $time_zone;
+			if ($pw1 != "") {
+				// Is attempting password change
+				if($pw1 == $pw2) {
+					$password = $db->password_encrypt($pw2);
+					$current_user->user_password = $password;
+				} else { // Passwords don't match
+					$message .= "Passwords do not match";
+				}
+			}
 			$current_user->update();
-			$message = "Okay, your password was updated";
-		} else { // Passwords don't match
-			$message = "Passwords do not match";
+			$message = "Changes saved.";
+		} else {
+			$message = "Please enter your current password in order to make changes.";
 		}
 	}
 ?>
@@ -23,9 +39,9 @@
 <h2>User Dashboard</h2>
 <form action="user-dashboard.php" method="post">
 <?php if($message) echo "<p>".$message."</p>"; ?>
+<div class="panel">
+<h3>User Info</h3>
 <p>User Name: <input name="user_name" value="<?php echo $current_user->user_name; ?>"></p>
-<p>Password: <input name="user_password" type="password"></p>
-<p>Password: (enter again) <input name="user_password_confirm" type="password"></p>
 <p>Time Zone: <select name="user_time_zone">
 	<?php foreach($all_time_zones as $time_zone) {
 		echo "<option value='{$time_zone}'";
@@ -35,7 +51,13 @@
 		echo ">{$time_zone}</option>";
 	} ?>
 </select></p>
+</div>
+<div class="panel">
+<h3>Change Password</h3>
+<p>New Password: <input name="user_password" type="password"></p>
+<p>New Password: <input name="user_password_confirm" type="password" placeholder="please re-enter"></p>
+</div>
+<p>Current Password: <input name="user_old_password" type="password" placeholder="required to edit"></p>
 <p><input type="submit" name="user_edited" value="Edit User Information"></p>
 </form>
-<p>Password: <?php echo $current_user->user_password; ?></p>
 <?php include_layout_template('footer.php'); ?>
