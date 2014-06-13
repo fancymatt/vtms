e969101dffdf2937f61e63415b435067deb6d4b7
Matt Henry <henrymatt@me.com>
2014-06-13 10:59:06 +0900

    Adds lesson top bar to lesson script page

--- public/lesson-script.php
+++ public/lesson-script.php
@@ -50,139 +50,142 @@
 ?>
 
 <?php include_layout_template('header.php'); ?>
-		<p><a href="lesson.php?series=<?php echo $lesson->series_id; ?>&langSeries=<?php echo $lesson->language_series_id; ?>&lesson=<?php echo $lesson->id; ?>">Return to Lesson Page</a></p>
-		<?php foreach($assets as $asset): ?>
-			<div class="panel">
-				<h3><?php echo $asset->task_name; ?></h3>
-				<p>Total Shots: <?php echo count(Shot::find_all_shots_for_asset($asset->id)); ?></p>
-				<p>Total Done: <?php echo count(Shot::find_all_completed_shots_for_asset($asset->id)); ?></p>
-			</div>
-		<?php endforeach; ?>
-		<div id="script">
-		<h2>Script</h2>
-		<form action="lesson-script.php?id=<?php echo $lesson->id; ?>" method="post">
-		<table class="script" id="lesson-script">
-			<th>Asset</th><th>Section</th><th>Shot</th><th>Script</th><th>Script English</th>
-			<?php foreach($shots as $shot): ?>
-					<tr>
-						<td>
-							<input type="hidden" name="shot_id[]" value="<?php echo $shot->id; ?>">
-							<select name="asset[]">
-							<option value="0">not set</option>
-							<?php foreach($assets as $asset) {
-								echo "<option value='{$asset->id}' ";
-								if($shot->asset_id == $asset->id) { echo "selected"; }
-								echo ">{$asset->task_name}</option>";
-							} ?>
-						</td>
-						<td><input type="text" name="section[]" size="10" value="<?php echo $shot->section; ?>"></td>
-						<td>
-							<input type="text" name="shot[]" size="3" value="<?php echo $shot->shot; ?>">
-							<select name="type[]">
-								<option value="">--</option>
-								<option value="CU" <?php if($shot->type=="CU") { echo "selected"; } ?> >CU</option>
-								<option value="WS" <?php if($shot->type=="WS") { echo "selected"; } ?> >WS</option>
-								<option value="SWS <?php if($shot->type=="SWS") { echo "selected"; } ?> ">SWS</option>
-								<option value="SCU <?php if($shot->type=="SCU") { echo "selected"; } ?> ">SCU</option>
-							</select>
-						</td>
-						<td><textarea name="script[]" rows="15" cols="35"><?php echo $shot->script; ?></textarea></td>
-						<td><textarea name="script_english[]" rows="15" cols="35"><?php echo $shot->script_english; ?></textarea></td>
-					</tr>
-				<?php endforeach; ?>
-				<tr><td><span onclick="addRow()">+ Row</span></td></tr>
-		</table>
-		<input type="submit" name="edited_script" value="Submit Changes">
-		</form>
+	<div id="lesson-header">
+		<h3><?php $lesson->display_full_lesson_navigation(); ?></h3>
+		<h4><?php $lesson->display_lesson_topbar("script"); ?></h4>
+	</div>
+	<?php foreach($assets as $asset): ?>
+		<div class="panel">
+			<h3><?php echo $asset->task_name; ?></h3>
+			<p>Total Shots: <?php echo count(Shot::find_all_shots_for_asset($asset->id)); ?></p>
+			<p>Total Done: <?php echo count(Shot::find_all_completed_shots_for_asset($asset->id)); ?></p>
+			<p><a href="record-asset.php?id=<?php echo $asset->id; ?>">Go to Recording Interface</a></p>
 		</div>
-		<p><a href="language-series.php?series=<?php echo $lesson->series_id; ?>&id=<?php echo $lesson->language_series_id; ?>"><- Return to Language Series List</a></p>
-		<script>
-		newRowIndex = 0;
+	<?php endforeach; ?>
+	<div id="script">
+	<form action="lesson-script.php?id=<?php echo $lesson->id; ?>" method="post">
+	<table class="script" id="lesson-script">
+		<th>Asset</th><th>Section</th><th>Shot</th><th>Script</th><th>Script English</th>
+		<?php foreach($shots as $shot): ?>
+				<tr>
+					<td>
+						<input type="hidden" name="shot_id[]" value="<?php echo $shot->id; ?>">
+						<select name="asset[]">
+						<option value="0">not set</option>
+						<?php foreach($assets as $asset) {
+							echo "<option value='{$asset->id}' ";
+							if($shot->asset_id == $asset->id) { echo "selected"; }
+							echo ">{$asset->task_name}</option>";
+						} ?>
+					</td>
+					<td><input type="text" name="section[]" size="10" value="<?php echo $shot->section; ?>"></td>
+					<td>
+						<input type="text" name="shot[]" size="3" value="<?php echo $shot->shot; ?>">
+						<select name="type[]">
+							<option value="">--</option>
+							<option value="CU" <?php if($shot->type=="CU") { echo "selected"; } ?> >CU</option>
+							<option value="WS" <?php if($shot->type=="WS") { echo "selected"; } ?> >WS</option>
+							<option value="SWS <?php if($shot->type=="SWS") { echo "selected"; } ?> ">SWS</option>
+							<option value="SCU <?php if($shot->type=="SCU") { echo "selected"; } ?> ">SCU</option>
+						</select>
+					</td>
+					<td><textarea name="script[]" rows="15" cols="35"><?php echo $shot->script; ?></textarea></td>
+					<td><textarea name="script_english[]" rows="15" cols="35"><?php echo $shot->script_english; ?></textarea></td>
+				</tr>
+			<?php endforeach; ?>
+			<tr><td><span onclick="addRow()">+ Row</span></td></tr>
+	</table>
+	<input type="submit" name="edited_script" value="Submit Changes">
+	</form>
+	</div>
+	<p><a href="language-series.php?series=<?php echo $lesson->series_id; ?>&id=<?php echo $lesson->language_series_id; ?>"><- Return to Language Series List</a></p>
+	<script>
+	newRowIndex = 0;
+	
+	function addRow() {
+		var table = document.getElementById("lesson-script");
+		var tableLength = document.getElementsByTagName("tr").length;
+		var row = table.insertRow(tableLength-1);
+		var cellAsset = row.insertCell(0);
+		var cellSection = row.insertCell(1);
+		var cellShot = row.insertCell(2);
+		var cellScript = row.insertCell(3);
+		var cellEnglish = row.insertCell(4);
 		
-		function addRow() {
-			var table = document.getElementById("lesson-script");
-			var tableLength = document.getElementsByTagName("tr").length;
-			var row = table.insertRow(tableLength-1);
-			var cellAsset = row.insertCell(0);
-			var cellSection = row.insertCell(1);
-			var cellShot = row.insertCell(2);
-			var cellScript = row.insertCell(3);
-			var cellEnglish = row.insertCell(4);
-			
-			var inputAsset = document.createElement("select");
-			inputAsset.name = "new_asset_id[]";
-			
-			<?php foreach($assets as $asset) { ?>
-				var option = document.createElement("option");
-				option.value = "<?php echo $asset->id; ?>";
-				option.innerHTML = "<?php echo $asset->task_name; ?>";
-				inputAsset.appendChild(option);
-			<?php } ?>
-			
-			var inputId = document.createElement("input");
-			inputId.type = "hidden";
-			inputId.name = "new_ID[]";
-			inputId.value = newRowIndex;
-			
-			var inputSection = document.createElement("input");
-			inputSection.type = "text";
-			inputSection.size = 10;
-			inputSection.name = "new_section[]";
-			
-			var inputShot = document.createElement("input");
-			inputShot.type = "text";
-			inputShot.size = 3;
-			inputShot.name = "new_shot[]";
-			
-			var inputType = document.createElement("select");
-			inputType.name = "new_type[]";
-			
-			var option1 = document.createElement("option");
-			option1.value = "";
-			option1.innerHTML = "--";
-			inputType.appendChild(option1);
-			
-			var option2 = document.createElement("option");
-			option2.value = "CU";
-			option2.innerHTML = "CU";
-			inputType.appendChild(option2);
-			
-			var option3 = document.createElement("option");
-			option3.value = "WS";
-			option3.innerHTML = "WS";
-			inputType.appendChild(option3);
-			
-			var option4 = document.createElement("option");
-			option4.value = "SWS";
-			option4.innerHTML = "SWS";
-			inputType.appendChild(option4);
-			
-			var option5 = document.createElement("option");
-			option5.value = "SCU";
-			option5.innerHTML = "SCU";
-			inputType.appendChild(option5);
-			
-			var inputScript = document.createElement("textarea");
-			inputScript.rows = 15;
-			inputScript.cols = 35;
-			inputScript.name = "new_script[]";
-			
-			var inputEnglish = document.createElement("textarea");
-			inputEnglish.rows = 15;
-			inputEnglish.cols = 35;
-			inputEnglish.name = "new_script_english[]";
-			
-			cellAsset.appendChild(inputId);
-			cellAsset.appendChild(inputAsset);
-			cellSection.appendChild(inputSection);
-			cellShot.appendChild(inputShot);
-			cellShot.appendChild(inputType);
-			cellScript.appendChild(inputScript);
-			cellEnglish.appendChild(inputEnglish);
-			
-			newRowIndex++;
-		}
+		var inputAsset = document.createElement("select");
+		inputAsset.name = "new_asset_id[]";
 		
-		</script>
+		<?php foreach($assets as $asset) { ?>
+			var option = document.createElement("option");
+			option.value = "<?php echo $asset->id; ?>";
+			option.innerHTML = "<?php echo $asset->task_name; ?>";
+			inputAsset.appendChild(option);
+		<?php } ?>
+		
+		var inputId = document.createElement("input");
+		inputId.type = "hidden";
+		inputId.name = "new_ID[]";
+		inputId.value = newRowIndex;
+		
+		var inputSection = document.createElement("input");
+		inputSection.type = "text";
+		inputSection.size = 10;
+		inputSection.name = "new_section[]";
+		
+		var inputShot = document.createElement("input");
+		inputShot.type = "text";
+		inputShot.size = 3;
+		inputShot.name = "new_shot[]";
+		
+		var inputType = document.createElement("select");
+		inputType.name = "new_type[]";
+		
+		var option1 = document.createElement("option");
+		option1.value = "";
+		option1.innerHTML = "--";
+		inputType.appendChild(option1);
+		
+		var option2 = document.createElement("option");
+		option2.value = "CU";
+		option2.innerHTML = "CU";
+		inputType.appendChild(option2);
+		
+		var option3 = document.createElement("option");
+		option3.value = "WS";
+		option3.innerHTML = "WS";
+		inputType.appendChild(option3);
+		
+		var option4 = document.createElement("option");
+		option4.value = "SWS";
+		option4.innerHTML = "SWS";
+		inputType.appendChild(option4);
+		
+		var option5 = document.createElement("option");
+		option5.value = "SCU";
+		option5.innerHTML = "SCU";
+		inputType.appendChild(option5);
+		
+		var inputScript = document.createElement("textarea");
+		inputScript.rows = 15;
+		inputScript.cols = 35;
+		inputScript.name = "new_script[]";
+		
+		var inputEnglish = document.createElement("textarea");
+		inputEnglish.rows = 15;
+		inputEnglish.cols = 35;
+		inputEnglish.name = "new_script_english[]";
+		
+		cellAsset.appendChild(inputId);
+		cellAsset.appendChild(inputAsset);
+		cellSection.appendChild(inputSection);
+		cellShot.appendChild(inputShot);
+		cellShot.appendChild(inputType);
+		cellScript.appendChild(inputScript);
+		cellEnglish.appendChild(inputEnglish);
+		
+		newRowIndex++;
+	}
+	
+	</script>
 
 <?php include_layout_template('footer.php'); ?>
