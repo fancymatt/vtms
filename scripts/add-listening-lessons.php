25796bce89f8018d426c389c6f7186fc9dda23eb
Matt Henry <henrymatt@me.com>
2014-06-16 14:37:29 +0900

    Updated add-listening-lessons script and adds rename-listening-lessons script

--- scripts/add-listening-lessons.php
+++ scripts/add-listening-lessons.php
@@ -4,21 +4,25 @@
 $session->confirm_logged_in();
 	
 	// Language Series which need the lessons applied to them
+
 /*
-	$language_series_abs_ids = array(90, 91, 92, 93, 94, 75, 64, 66, 95, 96, 97, 80, 72, 74, 98, 99, 100, 101, 102);
-	$abs_unit2_titles = array("6" => "Seeing a Movie in <country>", 
-							  "7" => "Shopping for a Shirt in <country>",
-							  "8" => "Ordering a Burger in <language>", 
-							  "9" => "Baking a Cake in <country>", 
-							  "10" => "Making Plans for the Day in <language>");
-	$due_dates = array("2014-05-27", "2014-06-03", "2014-06-10", "2014-06-17", "2014-06-24");
-	foreach($language_series_abs_ids as $language_series_id) {
+	//$language_series_abs_ids = array(90, 91, 92, 93, 94, 75, 64, 66, 95, 96, 97, 80, 72, 74, 98, 99, 100, 101, 102);
+	$language_series_beg_ids = array(79, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134);
+	$language_series_int_ids = array(152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183);
+	$language_series_adv_ids = array(199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230);
+	$unit_titles = array("6" => "Deciding on a Hotel in <country>", 
+						 "7" => "Setting up a Meeting Room in <country>",
+						 "8" => "Ordering Office Supplies in <language>", 
+						 "9" => "Getting to the Airport in <country>", 
+						 "10" => "Talking to a Supplier in <language>");
+	$due_dates = array("2014-12-23", "2015-01-13", "2015-01-27", "2015-02-10", "2015-02-24");
+	foreach($language_series_adv_ids as $language_series_id) {
 		// Create 5 lessons with the correct names and add them to the series
 		// Get language
 		$language_series = LanguageSeries::find_by_id($language_series_id);
 		$language = Language::find_by_id($language_series->language_id);
 		$i=0;
-		foreach ($abs_unit2_titles as $num => $title) {	
+		foreach ($unit_titles as $num => $title) {	
 			$new_lesson_name_pass1 = str_replace("<language>", $language->name, $title);
 			$new_lesson_name_pass2 = str_replace("<country>", $language->country_name, $new_lesson_name_pass1);
 			$new_lesson_name = $new_lesson_name_pass2;
@@ -45,8 +49,6 @@ $session->confirm_logged_in();
 		}
 	}
 */
-?>
 
-<?php include_layout_template('header.php'); ?>
+?>
 	Done!
-<?php include_layout_template('footer.php'); ?>
