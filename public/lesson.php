df6a41b568f1125c2ea4ca89806fad79de4c79dd
Matt Henry <henrymatt@me.com>
2014-06-18 18:03:57 +0900

    Integrates issues-for-lesson.php into lesson.php

--- public/lesson.php
+++ public/lesson.php
@@ -3,6 +3,9 @@
 	confirm_logged_in();
 	$lesson_id = $db->escape_value($_GET['id']);
 	$lesson = Lesson::find_by_id($lesson_id);
+	if (!$lesson->title) {
+		redirect_to("qa.php");
+	}
 	$language_series = LanguageSeries::find_by_id($lesson->language_series_id);
 	$series = Series::find_by_id($language_series->series_id);
 	
@@ -18,7 +21,16 @@
 		$lesson->qa_log = $_POST['edited_qa_log'];
 		$lesson->qa_url = $_POST['edited_qa_url'];
 		$lesson->update();
-		$message = "Updated";
+		$message = "Lesson details updated";
+	}
+	
+	if($_POST['changed_qa_log']) {
+		$qa_lesson_id = $_POST['qa_lesson_id'];
+		$qa_log = $_POST['qa_log'];
+		$lesson = Lesson::find_by_id($qa_lesson_id);
+		$lesson->qa_log = $qa_log;
+		$lesson->update();
+		$message = "You've updated the QA Log to: '" . $qa_log . "'";
 	}
 	
 	$trt = $lesson->trt;
@@ -27,8 +39,11 @@
 	
 	$tasks = Task::find_all_tasks_for_lesson($lesson_id);
 	$assets = Task::find_all_assets_for_lesson($lesson_id);
+	$tasks_and_assets = Task::find_all_assets_and_tasks_for_lesson($lesson->id);
 	$shots = Shot::find_all_shots_for_lesson($lesson_id);
-	$issues = Issue::get_unfinished_issues_for_lesson($lesson_id);
+	$unfinished_issues = Issue::get_unfinished_issues_for_lesson($lesson_id);
+	$all_issues = Issue::get_all_issues_for_lesson($lesson->id);
+
 ?>
 
 <?php include_layout_template('header.php'); ?>
@@ -55,6 +70,13 @@
 			<h3><?php echo $language_series->language_series_title." (".ucwords($language_series->level_code).")"; ?></h3>
 		</header>
 	</div>
+	
+	<?php if($message) { ?>
+	<div data-alert class="alert-box">
+	  <?php echo $message; ?>
+	  <a href="#" class="close">&times;</a>
+	</div>
+	<?php } ?>
 	
 	<div id="tabs" class="row">
 		<ul class="tabs" data-tab>
@@ -168,50 +190,102 @@
 					<div class="medium-11 medium-margin-1 small-12 columns">
 						<p><a href="lesson-script.php?id=<?php echo $lesson->id; ?>">Go to full script page</a></p>
 						<table class="script">
-						<thead>
-							<th>Section</th>
-							<th>Shot</th>
-							<th>Script</th>
-							<th>Script English</th>
-						</thead>
-						<tbody>
-							<?php if($shots) { ?>
-								<?php foreach($shots as $shot): ?>
+							<thead>
+								<th>Section</th>
+								<th>Shot</th>
+								<th>Script</th>
+								<th>Script English</th>
+							</thead>
+							<tbody>
+								<?php if($shots) { ?>
+									<?php foreach($shots as $shot): ?>
+										<tr>
+											<td><?php echo $shot->section; ?></td>
+											<td><?php echo $shot->shot." - ".$shot->type; ?></td>
+											<td><?php echo nl2br($shot->script); ?></td>
+											<td><?php echo nl2br($shot->script_english); ?></td>
+										</tr>
+									<?php endforeach; ?>
+								<?php } else { ?> <!-- End of if($shots) -->
 									<tr>
-										<td><?php echo $shot->section; ?></td>
-										<td><?php echo $shot->shot." - ".$shot->type; ?></td>
-										<td><?php echo nl2br($shot->script); ?></td>
-										<td><?php echo nl2br($shot->script_english); ?></td>
+										<td colspan="5">
+											<a href="lesson-script.php?id=<?php echo $lesson->id; ?>">No script. Click to edit.</a>
+										</td>
 									</tr>
-								<?php endforeach; ?>
-							<?php } else { ?> <!-- End of if($shots) -->
-								<tr>
-									<td colspan="5">
-										<a href="lesson-script.php?id=<?php echo $lesson->id; ?>">No script. Click to edit.</a>
-									</td>
-								</tr>
-							<?php } ?>
-						</tbody>
-					</table>
+								<?php } ?>
+							</tbody>
+						</table>
 					</div>
 				</div>
 			</div>
 		</div>
-		
 		<div class="content" id="panel-issues">
 			<div id="section-header" class="row">
-				<header class="medium-10 medium-margin-1 columns">
+				<header>
 					<h4>Issues</h4>
 				</header>
 			</div>
-			<div id="issues-list-table" class="row">
-				<div class="medium-11 medium-margin-1 small-12 columns">
-					<?php $lesson->display_list_of_issues_with_link(); ?>
-					Add in contents of issues-for-lesson.php here
+			<div id="issues-list" class="row">
+					<table>
+						<thead>
+							<tr>
+								<th>Task</th>
+								<th>Creator</th>
+								<th>Timecode</th>
+								<th>Issue</th>
+								<th>Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach($all_issues as $issue): ?>
+							<?php $task_for_issue_id = Task::find_by_id($issue->task_id); ?>
+							<tr>
+								<td><?php echo $task_for_issue_id->task_name; ?></td>
+								<td><?php echo $issue->issue_creator; ?></td>
+								<td><?php echo $issue->issue_timecode; ?></td>
+								<td><?php echo $issue->issue_body; ?></td>
+								<td><?php echo $issue->is_completed ? "Finished" : "Incomplete"; ?></td>
+							</tr>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
 				</div>
-			</div>
+			<div id="qa-log" class="row">
+					<h3>Current QA Log</h3>
+					<form action='lesson.php?id=<?php echo $lesson->id; ?>' method='post'>
+					<input type='text' size=70 name='qa_log' value='<?php echo $lesson->qa_log; ?>'>
+					<input type='hidden' name='qa_lesson_id' value='<?php echo $lesson->id; ?>'>
+					<input type='submit' name='changed_qa_log'>
+					</form>
+				</div>
+			<div id="add-an-issue" class="row">
+					<h3>Add an Issue</h3>
+					<table>
+					<thead>
+						<tr>
+							<th width="400">Problem</th>
+							<th width="400">Task Name</th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php foreach($tasks_and_assets as $task) {
+						$global_task = GlobalTask::find_by_id($task->global_task_id);
+						if ($global_task->can_add_issues) { ?>
+							<tr>
+								<td>
+									<a href="issues-for-task.php?id=<?php echo $task->id; ?>">
+								<?php echo $global_task->issue_reporting_friendly_text; ?></a>
+								</td>
+								<td>
+									<?php echo $global_task->task_name; ?>
+								</td>
+							</tr>
+						<?php } ?>
+					<?php } ?>
+					</tbody>
+				</table>
+				</div>
 		</div>
-		
 		<div class="content" id="panel-edit">
 			<div id="section-header" class="row">
 				<header class="medium-10 medium-margin-1 columns">
