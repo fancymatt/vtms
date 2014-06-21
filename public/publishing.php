a7a2798e161aa7b8932e256c412d4516b1144527
Matt Henry <henrymatt@me.com>
2014-06-21 11:27:45 +0900

    Updates publishing.php for foundation and allows batch updating

--- public/publishing.php
+++ public/publishing.php
@@ -4,65 +4,75 @@
 		$_SESSION['message'] = "You need admin privileges to access this page.";
 		redirect_to('login.php');
 	}
-	if($_POST['changed_qa']) {
-		$qa_lesson_id = $db->escape_value($_POST['qa_lesson_id']);
-		$qa_log = $db->escape_value($_POST['qa_log']);
-		$qa_url = $db->escape_value($_POST['qa_url']);
-		$message = "QA Log changed to: " . $qa_log;
-		$message = "QA URL changed to: " . $qa_url;
-		$lesson = Lesson::find_by_id($qa_lesson_id);
-		$lesson->qa_log = $qa_log;
-		$lesson->qa_url = $qa_url;
-		$lesson->update();
-	}
 	
-	if($_POST['marked_lesson_language_checked']) {
-		$qa_lesson_id = $db->escape_value($_POST['qa_lesson_id']);
-		$lesson = Lesson::find_by_id($qa_lesson_id);
-		$lesson->checked_language = 1;
-		$lesson->update();
-	}
-	
-	if($_POST['add_to_dropbox']) {
-		$dropbox_lesson_id = $db->escape_value($_POST['dropbox_lesson_id']);
-		$lesson = Lesson::find_by_id($dropbox_lesson_id);
-		$lesson->add_to_dropbox();
+	if($_POST["added_lesson_trt"]) {
+		$lesson_ids = $_POST['lesson_id'];
+		$lesson_trts = $_POST['lesson_trt'];
+		$i = 0;
+		$changed_records = 0;
+		if($lesson_ids) {
+			foreach($lesson_ids as $lesson_id) {
+				$lesson = Lesson::find_by_id($lesson_id);
+				
+				if($lesson_trts[$i]) {
+					//  & preg_match("^[0-9]+:[0-9]{2}$", $lesson_trts[$i]
+					$string_parts = explode (":", $lesson_trts[$i]);
+					$mins = $string_parts[0];
+					$secs = $string_parts[1];
+					$trt = $secs + ($mins*60);
+					if(is_int($trt)) {
+						$lesson->trt = $trt;
+						$lesson->update();
+						$changed_records++;
+					}
+				}
+				$i++;
+			}
+		}
+		$message = "TRT for " . $changed_records . " lessons have been updated.";
 	}
 	
 	$recently_completed_lessons = Lesson::find_all_recently_completed_lessons();
-	
-	$sort_by = $db->escape_value($_GET['sort']);	
-	$qa_lessons = Lesson::find_all_checkable_lessons($sort_by);
 ?>
 
 <?php include_layout_template('header.php'); ?>
-		<h2 class="main_title">Publishing</h2>
-		<?php if ($message) {
-			echo "<p>{$message}</p>";
-		} ?>
-	<div id="recently-finished-lessons">
-	<h3>Finished, no time added</h3>
-		<table>
-			<tr><th>Lesson</th><th>TRT</th><th>Actions</th></tr>
+	<div id="page-header" class="row">
+		<div class="small-12 columns">
+			<h3>Recently Completed Lessons</h3>
+		</div>
+	</div>
+	
+	<?php if($message) { ?>
+	<div data-alert class="alert-box">
+	  <?php echo $message; ?>
+	  <a href="#" class="close">&times;</a>
+	</div>
+	<?php } ?>
+	
+	<div id="recently-finished-lessons" class="row">
+		<div class="small-12 columns">
+			<form action="publishing.php" method="post">
+			<table>
+				<tr><th>Lesson</th><th width="400">TRT</th></tr>
 				<?php 
-				if(!$qa_lessons) {
+				if(!$recently_completed_lessons) {
 					echo "<td>No lessons</td>";
 				} else {
-					foreach($recently_completed_lessons as $lesson) {
-						echo "<tr>";
-						echo "<td>";
-						echo $lesson->display_full_lesson();
-						echo "</td>";
-						echo "<td>";
-						echo $lesson->trt;
-						echo "</td>";
-						echo "<td>";
-						echo "<a href='lesson.php?series={$lesson->series_id}&langSeries={$lesson->language_series_id}&lesson={$lesson->id}'>View</a>";
-						echo "</td>";
-						echo "</tr>";
-					} 
-				 } ?>		
-		</table>
+					foreach($recently_completed_lessons as $lesson) { ?>
+						<tr>
+							<td>
+								<?php echo $lesson->display_full_lesson(); ?>
+							</td>
+							<td>
+								<input name="lesson_trt[]" type="text" placeholder="ex. 3:45">
+								<input name="lesson_id[]" type="hidden" value="<?php echo $lesson->id; ?>">
+							</td>
+						</tr>
+				<?php }
+				} ?>		
+			</table>
+			<input type="submit" name="added_lesson_trt" class="button">
+		</div>
 	</div>
 		
 <?php include_layout_template('footer.php'); ?>
