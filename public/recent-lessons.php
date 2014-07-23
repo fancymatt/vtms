93306d71be92818ca6dcd1bcd74c3e076bdd21be
henrymatt <henrymatt@me.com>
2014-07-23 16:19:57 +0900

    Initial conversion of recent-lessons.php to foundation

--- public/recent-lessons.php
+++ public/recent-lessons.php
@@ -6,6 +6,54 @@
 ?>
 
 <?php include_layout_template('header.php'); ?>
+
+    <div class="row">
+  		<div class="small-12 columns">
+  		  <h3>Lesson Admin</h3>
+  		</div>
+		</div>
+
+    <div id="upcoming-lessons" class="small-12 columns">
+  		<h3 class="group-heading">Upcoming Lessons</h3>
+      <?php
+      if($upcoming_lessons) { ?>
+      <ol class="group">
+      <?php
+      foreach($upcoming_lessons as $lesson) : ?>
+        <div class="group-item">
+  				<div class="lesson-info">
+    				<p class="lesson-title"><?php echo $lesson->display_full_lesson(); ?></p>
+    				<p class="lesson-title"><?php echo $lesson->title; ?></p>
+    				<p class="date"><?php echo "Due ".$lesson->publish_date; ?></p>
+  				</div>
+  				<div class="lesson-status">
+  				  <p class="lesson-status-item">
+  				    <img src="<?php echo $lesson->is_shot ? "img/lesson-status-yes-shot.png" : "img/lesson-status-not-shot.png"?>">
+  				  </p>
+  				  <p class="lesson-status-item">
+  				    <img src="<?php echo $lesson->is_checkable ? "img/lesson-status-yes-checkable.png" : "img/lesson-status-not-checkable.png"?>">
+  				  </p>
+  				  <p class="lesson-status-item">
+  				    <img src="<?php echo $lesson->checked_language ? "img/lesson-status-yes-language.png" : "img/lesson-status-not-language.png"?>">
+  				  </p>
+  				  <p class="lesson-status-item">
+  				    <img src="<?php echo $lesson->checked_video ? "img/lesson-status-yes-video.png" : "img/lesson-status-not-video.png"?>">
+  				  </p>
+  				  <p class="lesson-status-item">
+  				    <img src="<?php echo $lesson->files_moved ? "img/lesson-status-yes-moved.png" : "img/lesson-status-not-moved.png"?>">
+  				  </p>
+  				</div>
+  				<div class="actions">
+  				  <a class="action-item" href="#"><img src="img/icon-add-issue.png"></a>
+  					<a class="action-item" href="#"><img src="img/icon-add-issue.png"></a>
+  				</div>
+    		</div>
+      <?php endforeach; ?>
+      </ol>
+      <?php } ?>
+    </div>
+
+
 		<div id="upcoming_lessons">
 		<h2 id="main_title">Upcoming Lessons</h2>
 		<?php if ($message) {
@@ -14,7 +62,8 @@
 		<table>
 			<tr><th>Lesson</th><th>Status</th><th>View</th><th>Due Date</th></tr>
 				<?php 
-				if(!$upcoming_lessons) {
+				/*
+if(!$upcoming_lessons) {
 					echo "<td>No lessons</td>";
 				} else {
 					foreach($upcoming_lessons as $upcoming_lesson) {
@@ -52,7 +101,8 @@
 						echo "<td>{$upcoming_lesson->publish_date}</td>";
 						echo "</tr>";
 					} 
-				 } ?>		
+				 }
+*/ ?>		
 		</table>
 		</div>
 <?php include_layout_template('footer.php'); ?>
