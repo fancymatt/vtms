e099b6d4381294e0ae81f59241336ceac4f09138
henrymatt <henrymatt@me.com>
2014-07-23 14:11:00 +0900

    Adds member image and fancy CSS to task display

--- public/tasks.php
+++ public/tasks.php
@@ -12,26 +12,32 @@
   
     <?php if($active_tasks) { ?>
       <div id="active-tasks" class="small-6 medium-10 medium-centered columns">
-  		<h3 class="lesson-group-heading">Active Tasks</h3>
-      <ol class="lesson-group">
+  		<h3 class="task-group-heading">Active Tasks</h3>
+      <ol class="task-group">
       <?php
       foreach($active_tasks as $task) : ?>
-        <div class="lesson">
-          <div class="lesson-member">
-    				<?php if($session->is_admin()) {
-  				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
-  				  } else {
-    				  echo $task->team_member_name;
-  				  } ?>
+        <div class="task">
+          <div class="member">
+            <div class="member-image">
+              <img src="img/headshot-<?php echo strtolower($task->team_member_name); ?>.png">
+
+            </div>
+            <p class="member-name">
+      				<?php if($session->is_admin()) {
+    				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
+    				  } else {
+      				  echo $task->team_member_name;
+    				  } ?>
+            </p>
   				</div>
-  				<div class="lesson-info">
+  				<div class="task-info">
     				<p class="lesson-title"><?php echo $task->display_full_task_lesson(); ?></p>
     				<p class="task-title"><?php echo $task->task_name; ?></p>
-    				<p class="lesson-due-date"><?php echo "Activated ".$logged_in_user->local_time($task->activated_time); ?></p>
+    				<p class="task-due-date"><?php echo "Activated ".$logged_in_user->local_time($task->activated_time); ?></p>
   				</div>
-  				<div class="lesson-actions">
-  				  <a class="lesson-action-item" href="#"><img src="img/icon-add-issue.png"></a>
-  					<a class="lesson-action-item" href="#"><img src="img/icon-add-issue.png"></a>
+  				<div class="task-actions">
+  				  <a class="task-action-item" href="#"><img src="img/icon-add-issue.png"></a>
+  					<a class="task-action-item" href="#"><img src="img/icon-add-issue.png"></a>
   				</ul>
     			</div>
     		</div>
@@ -41,28 +47,34 @@
 		<?php } ?>
 		
 		<div id="recent-tasks" class="medium-6 small-12 columns">
-  		<h3 class="lesson-group-heading">Recent Completed Tasks</h3>
+  		<h3 class="task-group-heading">Recent Completed Tasks</h3>
       <?php
       if($recent_tasks) { ?>
-      <ol class="lesson-group">
+      <ol class="task-group">
       <?php
       foreach($recent_tasks as $task) : ?>
-        <div class="lesson">
-          <div class="lesson-member">
-    				<?php if($session->is_admin()) {
-  				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
-  				  } else {
-    				  echo $task->team_member_name;
-  				  } ?>
+        <div class="task">
+          <div class="member">
+            <div class="member-image">
+              <img src="img/headshot-<?php echo strtolower($task->team_member_name); ?>.png">
+
+            </div>
+            <p class="member-name">
+      				<?php if($session->is_admin()) {
+    				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
+    				  } else {
+      				  echo $task->team_member_name;
+    				  } ?>
+            </p>
   				</div>
-  				<div class="lesson-info">
+  				<div class="task-info">
     				<p class="lesson-title"><?php echo $task->display_full_task_lesson(); ?></p>
     				<p class="task-title"><?php echo $task->task_name; ?></p>
-    				<p class="lesson-due-date"><?php echo "Completed ".$logged_in_user->local_time($task->completed_time); ?></p>
+    				<p class="task-due-date"><?php echo "Completed ".$logged_in_user->local_time($task->completed_time); ?></p>
   				</div>
-  				<div class="lesson-actions">
-  				  <a class="lesson-action-item" href="#"><img src="img/icon-add-issue.png"></a>
-  					<a class="lesson-action-item" href="#"><img src="img/icon-add-issue.png"></a>
+  				<div class="task-actions">
+  				  <a class="task-action-item" href="#"><img src="img/icon-add-issue.png"></a>
+  					<a class="task-action-item" href="#"><img src="img/icon-add-issue.png"></a>
   				</ul>
     			</div>
     		</div>
@@ -72,27 +84,32 @@
       </div>
 		
 		<div id="actionable-tasks" class="medium-6 small-12 columns">
-  		<h3 class="lesson-group-heading">Actionable Tasks</h3>
+  		<h3 class="task-group-heading">Actionable Tasks</h3>
       <?php
       if($actionable_tasks) { ?>
-      <ol class="lesson-group">
+      <ol class="task-group">
       <?php
       foreach($actionable_tasks as $task) : ?>
-        <div class="lesson">
-          <div class="lesson-member">
-  				<?php if($session->is_admin()) {
-				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
-				  } else {
-  				  echo $task->team_member_name;
-				  } ?>
+        <div class="task">
+          <div class="member">
+            <div class="member-image">
+              <img src="img/headshot-<?php echo strtolower($task->team_member_name); ?>.png">
+            </div>
+            <p class="member-name">
+      				<?php if($session->is_admin()) {
+    				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
+    				  } else {
+      				  echo $task->team_member_name;
+    				  } ?>
+            </p>
   				</div>
-  				<div class="lesson-info">
+  				<div class="task-info">
     				<p class="lesson-title"><?php echo $task->display_full_task_lesson(); ?></p>
     				<p class="task-title"><?php echo $task->task_name; ?></p>
-    				<p class="lesson-due-date"><?php echo "Due ".$task->task_due_date; ?></p>
+    				<p class="task-due-date"><?php echo "Due ".$task->task_due_date; ?></p>
   				</div>
-          <div class="lesson-actions">
-  					<a class="lesson-action-item" href="#"><img src="img/icon-add-issue.png"></a>
+          <div class="task-actions">
+  					<a class="task-action-item" href="#"><img src="img/icon-add-issue.png"></a>
   				</ul>
           </div>
   			</div>
