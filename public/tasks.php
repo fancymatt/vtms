65dd538684500aeda065cdac1e9af2ca7ef3ecea
henrymatt <henrymatt@me.com>
2014-07-23 12:07:55 +0900

    Improves responsive design problems with lesson display page

--- public/tasks.php
+++ public/tasks.php
@@ -11,25 +11,30 @@
 <?php include_layout_template('header.php'); ?>
   
     <?php if($active_tasks) { ?>
-      <div id="active-tasks" class="small-12 columns">
+      <div id="active-tasks" class="small-6 medium-10 medium-centered columns">
   		<h3 class="lesson-group-heading">Active Tasks</h3>
       <ol class="lesson-group">
       <?php
       foreach($active_tasks as $task) : ?>
-        <li class="lesson">
-  				<p class="lesson-title"><?php echo $task->display_full_task_lesson()." - ".$task->task_name; ?></p>
-  				<p class="lesson-member">
-  				<?php if($session->is_admin()) {
-				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
-				  } else {
-  				  echo $task->team_member_name;
-				  } ?>
-  				</p>
-  				<p class="lesson-due-date"><?php echo "Activated ".$logged_in_user->local_time($task->activated_time); ?></p>
-  				<ul class="lesson-actions">
-  					<li class="lesson-action-item"></li>
+        <div class="lesson">
+          <div class="lesson-member">
+    				<?php if($session->is_admin()) {
+  				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
+  				  } else {
+    				  echo $task->team_member_name;
+  				  } ?>
+  				</div>
+  				<div class="lesson-info">
+    				<p class="lesson-title"><?php echo $task->display_full_task_lesson(); ?></p>
+    				<p class="task-title"><?php echo $task->task_name; ?></p>
+    				<p class="lesson-due-date"><?php echo "Activated ".$logged_in_user->local_time($task->activated_time); ?></p>
+  				</div>
+  				<div class="lesson-actions">
+  				  <a class="lesson-action-item" href="#"><img src="img/icon-add-issue.png"></a>
+  					<a class="lesson-action-item" href="#"><img src="img/icon-add-issue.png"></a>
   				</ul>
-  			</li>
+    			</div>
+    		</div>
       <?php endforeach; ?>
       </ol>
 		</div>
@@ -42,24 +47,29 @@
       <ol class="lesson-group">
       <?php
       foreach($recent_tasks as $task) : ?>
-        <li class="lesson">
-  				<p class="lesson-title"><?php echo $task->display_full_task_lesson()." - ".$task->task_name; ?></p>
-  				<p class="lesson-member">
-  				<?php if($session->is_admin()) {
-				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
-				  } else {
-  				  echo $task->team_member_name;
-				  } ?>
-  				</p>
-  				<p class="lesson-due-date"><?php echo "Completed ".$logged_in_user->local_time($task->completed_time); ?></p>
-  				<ul class="lesson-actions">
-  					<li class="lesson-action-item"></li>
+        <div class="lesson">
+          <div class="lesson-member">
+    				<?php if($session->is_admin()) {
+  				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
+  				  } else {
+    				  echo $task->team_member_name;
+  				  } ?>
+  				</div>
+  				<div class="lesson-info">
+    				<p class="lesson-title"><?php echo $task->display_full_task_lesson(); ?></p>
+    				<p class="task-title"><?php echo $task->task_name; ?></p>
+    				<p class="lesson-due-date"><?php echo "Completed ".$logged_in_user->local_time($task->completed_time); ?></p>
+  				</div>
+  				<div class="lesson-actions">
+  				  <a class="lesson-action-item" href="#"><img src="img/icon-add-issue.png"></a>
+  					<a class="lesson-action-item" href="#"><img src="img/icon-add-issue.png"></a>
   				</ul>
-  			</li>
+    			</div>
+    		</div>
       <?php endforeach; ?>
       </ol>
       <?php } ?>
-		</div>
+      </div>
 		
 		<div id="actionable-tasks" class="medium-6 small-12 columns">
   		<h3 class="lesson-group-heading">Actionable Tasks</h3>
@@ -68,20 +78,24 @@
       <ol class="lesson-group">
       <?php
       foreach($actionable_tasks as $task) : ?>
-        <li class="lesson">
-  				<p class="lesson-title"><?php echo $task->display_full_task_lesson()." - ".$task->task_name; ?></p>
-  				<p class="lesson-member">
+        <div class="lesson">
+          <div class="lesson-member">
   				<?php if($session->is_admin()) {
 				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
 				  } else {
   				  echo $task->team_member_name;
 				  } ?>
-  				</p>
-  				<p class="lesson-due-date"><?php echo "Due ".$task->task_due_date; ?></p>
-  				<ul class="lesson-actions">
-  					<li class="lesson-action-item"></li>
+  				</div>
+  				<div class="lesson-info">
+    				<p class="lesson-title"><?php echo $task->display_full_task_lesson(); ?></p>
+    				<p class="task-title"><?php echo $task->task_name; ?></p>
+    				<p class="lesson-due-date"><?php echo "Due ".$task->task_due_date; ?></p>
+  				</div>
+          <div class="lesson-actions">
+  					<a class="lesson-action-item" href="#"><img src="img/icon-add-issue.png"></a>
   				</ul>
-  			</li>
+          </div>
+  			</div>
       <?php endforeach; ?>
       </ol>
       <?php } ?>
