dfe66f641b61c314508d87eb12f8e8559f0ac707
henrymatt <henrymatt@me.com>
2014-07-27 09:53:32 +0900

    Updates Assets page for foundation

--- public/recent-assets.php
+++ public/recent-assets.php
@@ -15,14 +15,14 @@
   		  <h3>Assets</h3>
   		</div>
 		</div>
-		
-		<?php if($deliverable_assets) { ?>
-		<div id="deliverable-assets" class="medium-8 medium-centered small-12 columns">
-  		<h3 class="group-heading">Deliverable Assets</h3>
-	      <ol class="group">
-	      <?php
-	      foreach($deliverable_assets as $task) : ?>
-	        <div class="group-item">
+		<div class="column">  		
+  		<?php if($deliverable_assets) { ?>
+  		<div id="deliverable-assets" class="medium-6 small-12 columns">
+    		<h3 class="group-heading">Deliverable Assets</h3>
+  	      <ol class="group">
+  	      <?php
+  	      foreach($deliverable_assets as $task) : ?>
+  	        <div class="group-item ready">
 	          <div class="member">
 	            <div class="member-image">
 	              <img src="img/headshot-<?php echo strtolower($task->team_member_name); ?>.png">
@@ -45,12 +45,47 @@
 	  					<a class="action-item" href="#"><img src="img/icon-add-issue.png"></a>
 	    			</div>
 	    		</div>
+  	      <?php endforeach; ?>
+  	      </ol>
+      </div>
+      <?php } ?>
+      <div id="actionable-assets" class="medium-6 small-12 columns">
+  		<h3 class="group-heading">Actionable Assets</h3>
+      <?php
+      if($actionable_assets) { ?>
+	      <ol class="group">
+	      <?php
+	      foreach($actionable_assets as $task) : ?>
+	        <div class="group-item<?php if(strtotime($task->task_due_date) < time()) { echo " overdue"; } ?>">
+	          <div class="member">
+	            <div class="member-image">
+	              <img src="img/headshot-<?php echo strtolower($task->team_member_name); ?>.png">
+	            </div>
+	            <p class="member-name">
+	      				<?php if($session->is_admin()) {
+	    				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
+	    				  } else {
+	      				  echo $task->team_member_name;
+	    				  } ?>
+	            </p>
+	  				</div>
+	  				<div class="task-info">
+	    				<p class="lesson-title"><?php echo $task->display_full_task_lesson(); ?></p>
+	    				<p class="task-title"><?php echo $task->task_name; ?></p>
+	    				<p class="date"><?php echo "Due ".$task->task_due_date; ?></p>
+	  				</div>
+	  				<div class="actions">
+	  				  <a class="action-item" href="#"><img src="img/icon-add-issue.png"></a>
+	  					<a class="action-item" href="#"><img src="img/icon-add-issue.png"></a>
+	    			</div>
+	    		</div>
 	      <?php endforeach; ?>
 	      </ol>
+	    <?php } ?>
     </div>
-    <?php } ?>
-		
-		<div id="active-assets" class="medium-6 small-12 columns">
+		</div>
+		<div class="column">
+		  <div id="recent-assets" class="medium-6 small-12 columns">
   		<h3 class="group-heading">Recent Assets</h3>
       <?php
       if($recent_assets) { ?>
@@ -84,40 +119,6 @@
 	      </ol>
 	    <?php } ?>
     </div>
-    
-    <div id="actionable-assets" class="medium-6 small-12 columns">
-  		<h3 class="group-heading">Actionable Assets</h3>
-      <?php
-      if($actionable_assets) { ?>
-	      <ol class="group">
-	      <?php
-	      foreach($actionable_assets as $task) : ?>
-	        <div class="group-item">
-	          <div class="member">
-	            <div class="member-image">
-	              <img src="img/headshot-<?php echo strtolower($task->team_member_name); ?>.png">
-	            </div>
-	            <p class="member-name">
-	      				<?php if($session->is_admin()) {
-	    				    echo "<a href='task-sheet.php?member={$task->team_member_id}'>{$task->team_member_name}</a>";  
-	    				  } else {
-	      				  echo $task->team_member_name;
-	    				  } ?>
-	            </p>
-	  				</div>
-	  				<div class="task-info">
-	    				<p class="lesson-title"><?php echo $task->display_full_task_lesson(); ?></p>
-	    				<p class="task-title"><?php echo $task->task_name; ?></p>
-	    				<p class="date"><?php echo "Due ".$task->task_due_date; ?></p>
-	  				</div>
-	  				<div class="actions">
-	  				  <a class="action-item" href="#"><img src="img/icon-add-issue.png"></a>
-	  					<a class="action-item" href="#"><img src="img/icon-add-issue.png"></a>
-	    			</div>
-	    		</div>
-	      <?php endforeach; ?>
-	      </ol>
-	    <?php } ?>
-    </div>
+		</div>
 
 <?php include_layout_template('footer.php'); ?>
