18d8ae1ee753188f4571034ac3982541fc9ff243
henrymatt <henrymatt@me.com>
2014-08-04 11:46:24 +0900

    Updates Operations for foundation

--- public/operations.php
+++ public/operations.php
@@ -4,6 +4,7 @@
 		$_SESSION['message'] = "You need admin privileges to access this page.";
 		redirect_to('login.php');
 	}
+	
 	if($_POST['changed_qa']) {
 		$qa_lesson_id = $db->escape_value($_POST['qa_lesson_id']);
 		$qa_log = $db->escape_value($_POST['qa_log']);
@@ -14,6 +15,8 @@
 		$lesson->qa_log = $qa_log;
 		$lesson->qa_url = $qa_url;
 		$lesson->update();
+		$_SESSION['message'] = "QA Info for ".$lesson->language_name." ".$lesson->series_name." #".$lesson->number. " is now: ".$qa_log." (".$qa_url.")";
+		redirect_to('operations.php');
 	}
 	
 	if($_POST['marked_lesson_language_checked']) {
@@ -52,65 +55,81 @@
 		</ul>
 	</div>
 
-	<div id="page-header" class="row">
-		<div class="small-12 columns">
-			<h3>ILL TV</h3>
-		</div>
-	</div>
-	
-	<?php if($message) { ?>
+  <?php if($message) { ?>
 	<div data-alert class="alert-box">
 	  <?php echo $message; ?>
 	  <a href="#" class="close">&times;</a>
 	</div>
 	<?php } ?>
-	
+
+	<div id="page-header" class="row">
+		<div class="small-12 columns">
+			<h3>Operations</h3>
+		</div>
+	</div>
 	<div id="panels" class="row">	
-		<div id="export-queue" class="panel small-12 medium-5 columns">
+		<div id="export-queue" class="panel small-12 medium-6 columns">
 			<p>Ready to Render: <strong><?php echo $exportable_lessons; ?></strong></p>
 			<p>Currently in Queue: <strong><?php echo $queued_lessons; ?></strong></p>
 			<p><a href="render-queue.php">Go to Render Page</a></p>
 		</div>
-		<div id="video-checking" class="panel">
+		<div id="video-checking" class="panel small-12 medium-6 columns">
 			<p>Files to Archive: <strong><?php echo $moveable_lessons; ?></strong></p>
 			<p>Ready to Check: <strong><?php echo $language_checked_lessons; ?></strong></p>
 			<p><a href="admin-video-check.php">Go to Video Check Page</a></p>
 		</div>
 	</div>
-	<div id="admin-qa" class="row">
-		<div id="get-these-checked">
-		<h3>Waiting for Language Check</h3>
-			<table>
-				<tr><th><a href='operations.php?sort=abc'>Lesson</a></th><th>QA Status</th><th>Actions</th><th><a href='operations.php?sort=export'>Exported Time</a></th><th><a href='operations.php?sort=pub'>Publish Date</a></th></tr>
-					<?php 
-					if(!$qa_lessons) {
-						echo "<td>No lessons</td>";
-					} else {
-						foreach($qa_lessons as $qa_lesson) {
-							$issues = Issue::get_unfinished_issues_for_lesson($qa_lesson->id);
-							
-							echo "<tr";
-							if (strpos($qa_lesson->qa_log, "Approved") !== false) {
-								echo " class='completed'";
-							}
-							echo ">";
-							echo "<td>";
-							echo $qa_lesson->display_full_lesson();
-							echo "</td>";
-							echo "<td><form action='operations.php?sort={$sort_by}' method='post'>";
-							echo "<input type='text' size=40 name='qa_log' value='{$qa_lesson->qa_log}'><br />";
-							echo "<input type='text' size=40 name='qa_url' value='{$qa_lesson->qa_url}'><br />";
-							echo "<input type='hidden' name='qa_lesson_id' value='{$qa_lesson->id}'><input type='submit' name='changed_qa'></form></td>";
-							echo "<td><form action='operations.php' method='post'>";
-							echo "Issues: ".count($issues);
-							echo "<input type='hidden' name='qa_lesson_id' value='{$qa_lesson->id}'><input type='submit' name='marked_lesson_language_checked' value='Mark Language Checked'></form></td>";
-							echo "<td>{$qa_lesson->exported_time}</td>";
-							echo "<td>{$qa_lesson->publish_date}</td>";
-							echo "</tr>";
-						} 
-					 } ?>		
-			</table>
-		</div>
+	<div class="row">
+    <div id="admin-qa" class="small-12 columns">
+  		<h3 class="group-heading">Waiting for Language Check</h3>
+      <?php
+      if($qa_lessons) { ?>
+      <ol class="group">
+      <?php
+      foreach($qa_lessons as $lesson) : ?>
+        <div class="group-item<?php if (strtotime($lesson->publish_date) < time()) { 
+                                      echo " overdue"; 
+                                      } else if (strpos(strtolower($lesson->qa_log), "approved") !== false) {
+                                      echo " ready";
+                                      } ?>">
+  		    <div class="lesson-info">
+    				<a class="lesson-title" href="lesson.php?id=<?php echo $lesson->id; ?>"><?php echo $lesson->display_full_lesson(); ?></a>
+    				<div class="qa-status">
+    				  <div class="small-2 columns">
+    				    <form action='operations.php' method='post'>
+      				  <label for="qa-log" class="inline">QA Log: </label>
+      				  <label for="qa-url" class="inline">QA URL: </label>
+    				  </div>
+    				  <div class="small-10 columns">
+      				  <input type="text" name="qa_log" size=40 value="<?php echo $lesson->qa_log; ?>">
+                <input type="text" name="qa_url" size=40 value="<?php echo $lesson->qa_url; ?>">
+    				  </div>
+    				</div>
+    				<p class="date"><?php echo "Due ".$lesson->publish_date; ?></p>
+    				<p class="date"><?php echo "Exported ".$lesson->exported_time; ?></p>
+  				</div>
+  				<div class="actions">
+  					<li class="action-item">
+							<input type='hidden' name='qa_lesson_id' value='<?php echo $lesson->id; ?>'>
+              <input value="Update QA" type="submit" class="no-format" name="changed_qa" data-tooltip class="has-tip" title="Update QA Log"><img src="img/lesson-status-yes-language.png"></button>
+  						</form>
+  					</li>
+    			</div>
+        </div>
+      <?php endforeach; ?>
+      </ol>
+      <?php } else { ?>
+        <div class="group-item">
+          <div class="lesson-info">
+            <a class="lesson-title" href="#">No lessons waiting on language check.</a>
+          </div>
+        </div>
+      <?php } ?>
+    </div>
 	</div>
+	
+	
+	
+	
 		
 <?php include_layout_template('footer.php'); ?>
