36200cac2a6c0c88497fa446990d0598746f6f96
henrymatt <henrymatt@me.com>
2015-01-26 12:59:58 +0900

    Makes text sizes larger

--- public/qa.php
+++ public/qa.php
@@ -40,18 +40,29 @@
 				</div>
 				<div class="qa-input">
   				<div class="row collapse">
-    				<form action="qa.php" method="post">
-            <div class="medium-5 large-5 columns">
+            <div class="small-6 medium-5 columns">
+              <form action="qa.php" method="post">
               <input type="text" name="qa_log" class="qa" placeholder="The current status of this video" value="<?php echo $lesson->qa_log; ?>">
             </div>
-            <div class="medium-5 large-5 columns">
+            <div class="small-6 medium-3 columns">
               <input type="text" name="qa_url" class="qa" placeholder="The URL of the checkable video" value="<?php echo $lesson->qa_url; ?>">
             </div>
-            <div class="medium-2 large-2 columns">
-              <input type="hidden" name="qa_lesson_id" value="<?php echo $lesson->id; ?>">
-              <input type="submit" name="changed_qa" class="button postfix" value="Update QA Log">
+            <div class="small-12 medium-4 columns">
+              <ul class="button-group">
+                <li>
+                  <input type="hidden" name="qa_lesson_id" value="<?php echo $lesson->id; ?>">
+                  <input type="submit" name="changed_qa" class="button" value="Update QA Log">
+                  </form>
+                </li>
+                <li>
+                  <a href="issues-for-lesson.php?id=<?php echo $lesson->id; ?>" class="button alert">Add Issue</a>
+                </li>
+              </ul>
+              
             </div>
-    				</form>
+            <div class="small-6 medium-10 columns">
+              
+            </div>
           </div>
   		  </div>
   		</div>
